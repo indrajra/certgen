001ce225b1b4092f3681fb1eeea9721ae1e74bda
aishwa8141 <a.aish1997@gmail.com>
2019-07-15 18:25:06 +0530

    testcases for ivaluators

--- certgen/pojos/src/main/java/org/incredible/pojos/ob/valuator/ExpiryDateValuator.java
+++ certgen/pojos/src/main/java/org/incredible/pojos/ob/valuator/ExpiryDateValuator.java
@@ -1,12 +1,13 @@
 package org.incredible.pojos.ob.valuator;
 
-import java.text.DateFormat;
+import org.incredible.pojos.ob.exeptions.InvalidDateFormatException;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class ExpiryDateValuator implements IEvaluator {
@@ -17,43 +18,76 @@ public class ExpiryDateValuator implements IEvaluator {
     }
 
     @Override
-    public String evaluates(Object inputVal) {
+    public String evaluates(Object inputVal) throws InvalidDateFormatException {
         return getExpiryDate((String) inputVal);
     }
 
 
-    public String getExpiryDate(String expiryDate) {
+    public String getExpiryDate(String expiryDate) throws InvalidDateFormatException {
+
+        /**
+         * regex of the date format yyyy-MM-dd'T'HH:mm:ss'Z'
+         */
         String pattern = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}Z";
         if (expiryDate.matches(pattern)) {
             return expiryDate;
         } else {
             try {
-                /**
-                 to split expiry dates of form (2m 2y)
-                 */
-                String[] splitExpiry = expiryDate.split(" ");
-                Calendar cal = Calendar.getInstance();
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-                Date parsedIssuedDate = simpleDateFormat.parse(issuedDate);
-                cal.setTime(parsedIssuedDate);
+                if (issuedDate == null) {
+                    throw new InvalidDateFormatException("Issued date is null, please provide valid issued date ");
+                } else {
+                    /**
+                     to split expiry dates of form (2m 2y)
+                     */
+                    String[] splitExpiry = expiryDate.split(" ");
+                    Calendar cal = Calendar.getInstance();
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                    Date parsedIssuedDate = simpleDateFormat.parse(issuedDate);
+                    cal.setTime(parsedIssuedDate);
 
-                for (int index = 0; index < splitExpiry.length; index++) {
-                    if (splitExpiry[index].endsWith("d") || splitExpiry[index].endsWith("D")) {
-                        cal.add(Calendar.DATE, Integer.parseInt(splitExpiry[index].substring(0, splitExpiry[index].length() - 1)));
-                    } else if (splitExpiry[index].endsWith("m") || splitExpiry[index].endsWith("M")) {
-                        cal.add(Calendar.MONTH, Integer.parseInt(splitExpiry[index].substring(0, splitExpiry[index].length() - 1)));
-                    } else if (splitExpiry[index].endsWith("y") || splitExpiry[index].endsWith("Y")) {
-                        cal.add(Calendar.YEAR, Integer.parseInt(splitExpiry[index].substring(0, splitExpiry[index].length() - 1)));
+                    for (int index = 0; index < splitExpiry.length; index++) {
+                        String string = checkValid(splitExpiry[index]);
+
+                        switch (string.toLowerCase()) {
+                            case "d":
+                                cal.add(Calendar.DATE, getDigits(splitExpiry[index]));
+                                break;
+                            case "m":
+                                cal.add(Calendar.MONTH, getDigits(splitExpiry[index]));
+                                break;
+                            case "y":
+                                cal.add(Calendar.YEAR, getDigits(splitExpiry[index]));
+                                break;
+                        }
                     }
+                    return simpleDateFormat.format(cal.getTime());
                 }
-                return simpleDateFormat.format(cal.getTime());
-
             } catch (ParseException e) {
                 e.printStackTrace();
                 return null;
             }
         }
+    }
 
+    private int getDigits(String string) {
+        Pattern pattern = Pattern.compile("^\\d+");
+        Matcher matcher = pattern.matcher(string);
+        if (matcher.find()) {
+            return Integer.parseInt(matcher.group(0));
+        } else {
+            return 0;
+        }
+
+    }
+
+    private String checkValid(String string) throws InvalidDateFormatException {
+        Pattern pattern = Pattern.compile("^\\d+[MYDmyd]{1}$");
+        Matcher matcher = pattern.matcher(string);
+        if (matcher.find()) {
+            return matcher.group(0).substring(matcher.group(0).length() - 1);
+        } else {
+            throw new InvalidDateFormatException("Given expiry date is invalid" + string);
+        }
 
     }
 }
